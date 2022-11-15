c0ab2b6b6d46ee42c9af22eb5517bf7ca2afbfd7
jeremycui2008 <54760319+OceanNut420@users.noreply.github.com>
2022-11-14 16:58:07 -0800

    I did string conversations 1, 2 and 3.

---
+++ src/_03_string_conversion/_1_intro/string_conversation.java
@@ -0,0 +1,34 @@
+package _03_string_conversion._1_intro;
+
+import javax.swing.JOptionPane;
+
+public class string_conversation {
+
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		String quanity = JOptionPane.showInputDialog(" Enter a number");
+		
+		int quanitynum = Integer.parseInt(quanity);
+		
+		System.out.println(quanity+5);
+		System.out.println(quanitynum+5);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
